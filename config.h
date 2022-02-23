f56d37430bde7850c325e36f9608abdf4a303c32
PercyTheB <iknowiamcool5@gmail.com>
2022-02-23 13:27:03 +0400

    changed colorscheme and font

--- config.h
+++ config.h
@@ -5,8 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Mononoki Nerd Font:pixelsize=16:antialias=true:autohint=true";
-static char *font2[] = { "Mononoki Nerd Font:pixelsize=16:antialias=true:autohint=true" };
+static char *font = "Mononoki Nerd Font:pixelsize=20:antialias=true:autohint=true";
+static char *font2[] = { "Mononoki Nerd Font:pixelsize=20:antialias=true:autohint=true" };
 static int borderpx = 0;
 
 /*
@@ -118,22 +118,22 @@ float alpha = 1.0;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
-  "#282828", /* hard contrast: #1d2021 / soft contrast: #32302f */
-  "#cc241d",
-  "#98971a",
-  "#d79921",
-  "#458588",
-  "#b16286",
-  "#689d6a",
-  "#a89984",
-  "#928374",
-  "#fb4934",
-  "#b8bb26",
-  "#fabd2f",
-  "#83a598",
-  "#d3869b",
-  "#8ec07c",
-  "#ebdbb2",
+  "#282c34",
+  "#e06c75",
+  "#98c379",
+  "#e5c07b",
+  "#61afef",
+  "#c678dd",
+  "#56b6c2",
+  "#abb2bf",
+  "#545862",
+  "#e06c75",
+  "#98c379",
+  "#e5c07b",
+  "#61afef",
+  "#c678dd",
+  "#56b6c2",
+  "#c8ccd4",
   [255] = 0,
   /* more colors can be added after 255 to use with DefaultXX */
   "#add8e6", /* 256 -> cursor */
